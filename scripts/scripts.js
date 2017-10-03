8e464d1890b9d15277f66ff6ade3be39e9bcf449
Stu Wares <stuwares@outlook.com>
2017-10-03 07:29:01 +0100

    adding jquery to access wikipedia api

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,29 @@
 // https://en.wikipedia.org/w/api.php - English wikipedia api end point
+
+$(document).ready(function(){
+
+	$("#search-button").on("click", function(){
+
+		var searchTerm = $("#search-input").val();
+
+		$.ajax({
+			type: "GET",
+			url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+			//url: "https://en.wikipedia.org/w/api.php",
+			contentType: "application/json; charset=utf-8",
+			async: false,
+			dataType: "json",
+			success: function (data, textStatus, jqXHR) {
+				console.log(data);
+			},
+			error: function(errorMessage){
+
+			}
+
+		})
+
+	})
+
+
+
+})
